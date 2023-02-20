fixes for model/weights loading | label extraction | kNN calc | normalization

pyscripts/extract_image_labels.py
@@ -324,7 +324,8 @@ if __name__ == '__main__':
     #compute CLS features and save them
     features, labels, image_names = extract_and_save_feature_pipeline(args)
 
-    # save features and labels
+
+    # save features and labels as files
     if dist.get_rank() == 0:
 
         #translate channel indexes to channel names
@@ -339,22 +340,10 @@ if __name__ == '__main__':
         channel_names = get_channel_name_combi(selected_channel_str, args.channel_dict)
 
         if args.scDINO_full_pipeline:
-            split_string = args.full_ViT_name.split("_epoch")
-            ViT_name = split_string[0]
-            epoch_num = split_string[1]
-            cls_directory = os.path.join(args.output_dir,args.name_of_run, f"{ViT_name}_analyses/CLS_features")
-            epoch_path = os.path.join(cls_directory, f"epoch{epoch_num}")
-            print("epoch path: ", epoch_path)
-            print("cls directory: ", cls_directory)
+            path = os.path.join(args.output_dir,args.name_of_run)+"/"
           
-        else:
-            cls_directory = os.path.join(args.output_dir,args.name_of_run,"CLS_features")
-            model_name = args.pretrained_weights.split('/')[-1].split('.')[0]
-            epoch_path = os.path.join(cls_directory, f"channel{channel_names}_model_{model_name}")
-        
-        np.savetxt(f"{epoch_path}_features.csv", features.cpu().numpy(), delimiter=",")
-        with open(os.path.join(cls_directory, "run_log.txt"), "w") as f:
-            f.write(f"computing features with seed {args.seed}: \n")
-            f.write("parameters: \n")
-            for arg in vars(args):
-                f.write(f"{arg} : {getattr(args, arg)} \n")
+        else: 
+             path = os.path.join(args.output_dir,args.name_of_run, f"CLS_features/")
+            
+        np.savetxt(f"{path}class_labels.csv", labels, delimiter=",", fmt="%s")
+        np.savetxt(f"{path}image_paths.csv", image_names, delimiter=",", fmt="%s")

pyscripts/global_kNN.py
@@ -29,7 +29,10 @@ for run_index, features in enumerate(snakemake.input["features"]):
             labels_numbers = le.transform(labels)
             return labels_numbers
 
-    #split into train and test and convert to torch tensors
+    indices = np.arange(features.shape[0])
+    np.random.shuffle(indices)
+    features = features[indices]
+    class_labels = np.array(class_labels)[indices]
     train_features = torch.from_numpy(features[:int(features.shape[0]*0.8),:])
     train_labels = torch.from_numpy(label_to_number(class_labels[:int(features.shape[0]*0.8)]))
     test_features = torch.from_numpy(features[int(features.shape[0]*0.8):,:])
@@ -72,7 +75,7 @@ for run_index, features in enumerate(snakemake.input["features"]):
             # find the predictions that match the target
             correct = predictions.eq(targets.data.view(-1, 1))
             top1 = top1 + correct.narrow(1, 0, 1).sum().item()
-            top2 = top2 + correct.narrow(1, 0, min(2, k)).sum().item()
+            top2 = top2 + correct.narrow(1, 0, min(2, k)).sum().item()  #dependent on number of classes
             total += targets.size(0)
         top1 = top1 * 100.0 / total
         top2 = top2 * 100.0 / total
@@ -85,6 +88,8 @@ for run_index, features in enumerate(snakemake.input["features"]):
                 top1, top2 = knn_classifier(train_features, train_labels,
                     test_features, test_labels, k, temperature, num_classes=num_classes)
                 f.write(f"{k}-NN Top1: {round(top1,2)}, Top2: {round(top2,2)}\n")
+                #delete variables
+                del top1, top2
             except Exception as e:
-                f.write("Error: "+str(e)+"\n")                
+                f.write("Error: "+str(e)+"\n")             
         f.write("--------------------------------------------------\n")

pyscripts/visualise_attention.py
@@ -155,7 +155,8 @@ if args.read_model_arch_dynamically:
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 # build model
 selected_channels = list(map(int, args.selected_channels))
-model = vits.__dict__[args.arch](patch_size=args.patch_size, num_classes=0, in_chans=len(selected_channels))
+num_in_chans_pretrained = utils.get_pretrained_weights_in_chans(args.pretrained_weights)
+model = vits.__dict__[args.arch](patch_size=args.patch_size, num_classes=0, in_chans=num_in_chans_pretrained)
 for p in model.parameters():
     p.requires_grad = False
 model.eval()
@@ -188,6 +189,12 @@ def build_weight_emb(embedding_seq, model):
     weights = weights[:,embedding_seq,:,:]
     model.patch_embed.proj.weight = nn.Parameter(weights)
     return model
+
+if not args.use_mean_patch_embedding and not args.use_custom_embedding_map:
+    if len(selected_channels) != num_in_chans_pretrained:
+        print(f"Error: Number of channels in the dataset ({len(selected_channels)}) and pretrained weights ({num_in_chans_pretrained}) are different")
+        print(f"Use --use_mean_patch_embedding or --use_custom_embedding_map to adjust the number of channels")
+        sys.exit(1)
 
 if not args.images_are_RGB:
     if args.use_mean_patch_embedding:
@@ -331,7 +338,13 @@ for class_name, class_number in classes_dict.items():
             w_featmap = img.shape[-2] // args.patch_size
             h_featmap = img.shape[-1] // args.patch_size
 
-            attentions = model.get_last_selfattention(img.to(device))
+            try:
+                attentions = model.get_last_selfattention(img.to(device))
+            ## print the erorr message
+            except Exception as e:
+                print("Error in getting attention images for image.shape: ", img.shape)
+                print(e)
+                continue
 
             nh = attentions.shape[1] # number of head
 
@@ -371,7 +384,10 @@ for class_name, class_number in classes_dict.items():
         
                 for j in range(nh):
                     display_instances(image, th_attn[j], fname=os.path.join(random_cell_dir, "mask_th" + str(args.threshold) + "_head" + str(j)+".png"), blur=False)
-
+            #free gpu memory
+            del img
+            del attentions
+    
 #creating log file
 with open(os.path.join(run_directory, "run_log.txt"), "w") as f:
     f.write(f"successfully computed attention visualisation with following parameters and random seed {args.seed}: \n")